c4c14dccb97dd1628efd57f7616bedd75994f96a
sadawi <hahli144@hotmail.com>
2020-04-30 15:38:35 +0300

    add backspace/delete, left/right movement

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/17 14:23:12 by sadawi            #+#    #+#             */
-/*   Updated: 2020/04/30 13:25:50 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/04/30 15:37:11 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,27 +39,108 @@ int		read_key(void)
 	return (c);
 }
 
+char	*str_remove_char(char *str, int index)
+{
+	int		i;
+	char	*newstr;
+
+	if (index < 0 || index >= (int)ft_strlen(str))
+		return (str);
+	newstr = (char*)malloc(ft_strlen(str));
+	i = 0;
+	while (i < index)
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	if (!str[i])
+	{
+		free(str);
+		return (newstr);
+	}
+	while (str[i + 1])
+	{
+		newstr[i] = str[i + 1];
+		i++;
+	}
+	newstr[i] = '\0';
+	free(str);
+	return (newstr);
+}
+
+char	*str_add_char(char *str, char c)
+{
+	int		i;
+	int		index;
+	char	*newstr;
+
+	index = ft_strlen(g_21sh->line) + g_21sh->cursor.x;
+	newstr = (char*)malloc(ft_strlen(str) + 2);
+	i = 0;
+	while (i < index)
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	newstr[i] = c;
+	while (str[i])
+	{
+		newstr[i + 1] = str[i];
+		i++;
+	}
+	newstr[i + 1] = '\0';
+	free(str);
+	return (newstr);
+}
+
+void	move_cursor_left(void)
+{
+	if ((int)ft_strlen(g_21sh->line) > -g_21sh->cursor.x)
+		g_21sh->cursor.x--;
+}
+
+void	move_cursor_right(void)
+{
+	if (g_21sh->cursor.x < 0)
+		g_21sh->cursor.x++;
+}
+
+void	handle_delete(void)
+{
+	g_21sh->line = str_remove_char(g_21sh->line, ft_strlen(g_21sh->line)
+	+ g_21sh->cursor.x);
+	move_cursor_right();
+}
+
 void	handle_control_sequence(char *c)
 {
 	*c += 100;
 	
 	if (*c == g_21sh->key_sequences.delete)
-	{
-		
-	}
+		handle_delete();
 	else if (*c == g_21sh->key_sequences.left_arrow)
-		g_21sh->cursor.x--;
+		move_cursor_left();
 	else if (*c == g_21sh->key_sequences.right_arrow)
-		g_21sh->cursor.x++;
+		move_cursor_right();
 	else if (*c == g_21sh->key_sequences.up_arrow)
 		;
 	else if (*c == g_21sh->key_sequences.down_arrow)
 	{
 			;
 	}
+	else if (*c == HOME)
+		g_21sh->cursor.x = -ft_strlen(g_21sh->line);
+	else if (*c == END)
+		g_21sh->cursor.x = 0;	
 }
 
-int		handle_keys(char **line)
+void	handle_backspace(void)
+{
+	g_21sh->line = str_remove_char(g_21sh->line, ft_strlen(g_21sh->line)
+	+ g_21sh->cursor.x - 1);
+}
+
+int		handle_keys(void)
 {
 	char c;
 
@@ -74,14 +155,14 @@ int		handle_keys(char **line)
 	}
 	if (c == BACKSPACE)
 	{
-
+		handle_backspace();
 	}
 	else if (c == ENTER)
 		return (0);
 	else if (c == 4)
 		exit(0); //temporary, need to restore terminal and free memory here
 	else
-		*line = ft_strjoinfree(*line, ft_chartostr(c));
+		g_21sh->line = str_add_char(g_21sh->line, c);
 	return (1);
 }
 
@@ -104,25 +185,22 @@ void	move_cursor(void)
 		set_terminal("le");
 }
 
-int		get_input(char **line)
+int		get_input()
 {
-	*line = ft_strnew(0);
-	while (handle_keys(line))
+	g_21sh->line = ft_strnew(0);
+	while (handle_keys())
 	{
 		move_cursor_start();
 		set_terminal("cd");
-		ft_printf("%s", *line);
+		ft_printf("%s", g_21sh->line);
 		move_cursor();
 	}
-	if (line[0] == 0)
-		return (0);
 	return (1);
 }
 
 void	loop_shell(void)
 {
 	char	**commands;
-	char	*line;
 	char	**args;
 	int		loop;
 	int		i;
@@ -132,10 +210,10 @@ void	loop_shell(void)
 	{
 		g_21sh->cursor.x = 0;
 		print_shell_info();
-		if (get_input(&line) < 1)
+		if (get_input() < 1)
 			break ;
 		ft_printf("\n");
-		commands = split_line_commands(line);
+		commands = split_line_commands(g_21sh->line);
 		i = 0;
 		while (commands[i])
 		{
@@ -145,7 +223,7 @@ void	loop_shell(void)
 			free(commands[i++]);
 		}
 		free(commands);
-		free(line);
+		free(g_21sh->line);
 	}
 }
 
