651caf8969db1de63c506955b19e060b06f15c95
Salah Adawi <salah.adawi@outlook.com>
2021-04-02 15:38:48 +0300

    renable terminal enviroment checks

--- srcs/termcaps/termcaps.c
+++ srcs/termcaps/termcaps.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   termcaps.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jochumwilen <jochumwilen@student.42.fr>    +#+  +:+       +#+        */
+/*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 17:00:00 by jwilen            #+#    #+#             */
-/*   Updated: 2021/04/02 12:50:17 by jochumwilen      ###   ########.fr       */
+/*   Updated: 2021/04/02 15:34:31 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ void	init_termcaps(void)
 	if (!(isatty(0)))
 		handle_error("Not a terminal", 0);
 	if (!(terminal_name = getenv("TERM")))
-		;//handle_error("Terminal enviroment variable not found", 0);
+		handle_error("Terminal enviroment variable not found", 0);
 	if (tgetent(NULL, terminal_name) < 1)
-		; //handle_error("Terminal specified in env not found", 0);
+		handle_error("Terminal specified in env not found", 0);
 	init_key_sequences();
 	tcgetattr(STDOUT_FILENO, &g_21sh->old);
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &g_21sh->window);
